f1728934bed6acbd4658880fff02068e2d452b69
MatteoRuffa <cortese_17@protonmail.com>
2024-06-11 20:00:26 +0200

    added API call and routes

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->get();
+        // dd($projects);
+        return response()->json([
+            'success' => true,
+            'message' => 'Ok',
+            'results' => $projects
+        ], 200);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+        if($project){
+            return response()->json([
+                'success' => true,
+                'message' => 'Ok',
+                'results' => $project
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Project not found'
+            ], 404);
+        }
+    }
+
+}
